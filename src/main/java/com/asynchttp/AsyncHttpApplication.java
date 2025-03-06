dec1b5064987fd857c7f40b5d8d00f74b3232497
ssol-park <ksttlov.naver@hanmail.net>
2025-03-06 22:32:52 +0900

    initialize

---
+++ src/main/java/com/asynchttp/AsyncHttpApplication.java
@@ -0,0 +1,13 @@
+package com.asynchttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncHttpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asynchttp/AsyncHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asynchttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncHttpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
