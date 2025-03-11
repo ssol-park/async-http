070b176b73454f7d31787ce57b94c25b850df30e
ssol-park <ksttlov.naver@hanmail.net>
2025-03-11 16:08:54 +0900

    feat: 단건 post 조회 테스트 추가

--- web-client-17/src/main/java/com/psr/webclient17/service/ApiService.java
+++ web-client-17/src/main/java/com/psr/webclient17/service/ApiService.java
@@ -2,13 +2,24 @@ package com.psr.webclient17.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class ApiService {
+
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    private static final String POSTS_ENDPOINT = "/posts";
+
     private final WebClient webClient;
 
     public ApiService(WebClient webClient) {
         this.webClient = webClient;
     }
 
+    public Mono<String> getSinglePost(int postId) {
+        return webClient.get()
+                .uri(BASE_URL + POSTS_ENDPOINT + "/{id}", postId)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
 }
--- web-client-17/src/test/java/com/psr/webclient17/ApiServiceTest.java
+++ web-client-17/src/test/java/com/psr/webclient17/ApiServiceTest.java
@@ -1,12 +1,32 @@
 package com.psr.webclient17;
 
 import com.psr.webclient17.service.ApiService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApiServiceTest {
 
     @Autowired
     private ApiService apiService;
+
+    @ParameterizedTest
+    @ValueSource(ints = {1})
+    void 단건_조회_테스트(int postId) {
+        Mono<String> singlePost = apiService.getSinglePost(postId);
+
+        StepVerifier.create(singlePost)
+                .assertNext(body -> {
+                    System.out.println("body = " + body);
+                    assertThat(body).isNotNull();
+                })
+                .verifyComplete();
+    }
 }
