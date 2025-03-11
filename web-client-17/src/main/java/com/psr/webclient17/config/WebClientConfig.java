6ff49ab19c4077258d2f78314520d5c72ec700a9
ssol-park <ksttlov.naver@hanmail.net>
2025-03-11 16:54:10 +0900

    feat: webClient tls 및 인증서 설정 샘플 추가

--- web-client-17/src/main/java/com/psr/webclient17/config/WebClientConfig.java
+++ web-client-17/src/main/java/com/psr/webclient17/config/WebClientConfig.java
@@ -1,8 +1,18 @@
 package com.psr.webclient17.config;
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 
 @Configuration
 public class WebClientConfig {
@@ -11,4 +21,33 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder().build();
     }
+
+    /* mTls 사용 시
+    public WebClient webClientSsl() throws Exception {
+        SslContext sslContext = createSslContext();
+
+        HttpClient httpClient = HttpClient.create()
+                .secure(ssl -> ssl.sslContext(sslContext));
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+
+    private SslContext createSslContext() throws Exception {
+        // KeyStore 로드 (JKS or PKCS12)
+        String keyStorePath = "keyPath..";
+        String keyStorePwd = "password..";
+
+        KeyStore keyStore = KeyStore.getInstance("pkcs12");
+        keyStore.load(new FileInputStream(keyStorePath), keyStorePwd.toCharArray());
+
+        TrustManagerFactory factory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        factory.init(keyStore);
+
+        return SslContextBuilder.forClient()
+                .protocols("TLSv1.3")
+                .trustManager(factory)
+                .build();
+    }*/
 }
