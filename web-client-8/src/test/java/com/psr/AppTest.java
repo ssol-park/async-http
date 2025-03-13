0b1135d911905b7e7950a2532da36555934c1281
ssol-park <ksttlov.naver@hanmail.net>
2025-03-13 16:41:46 +0900

    feat: WebClient 설정 및 테스트 추가

---
+++ web-client-8/src/test/java/com/psr/AppTest.java
@@ -0,0 +1,58 @@
+package com.psr;
+
+
+import com.psr.config.WebClientConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = WebClientConfig.class)
+class AppTest {
+    private static final Logger logger = LoggerFactory.getLogger(AppTest.class);
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    private static final AtomicInteger counter = new AtomicInteger(0);
+
+    @Autowired
+    private WebClient webClient;
+
+    static Stream<String> provideUrls() {
+        return IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> "/posts/" + i);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideUrls")
+    void WebClient_테스트(String url) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        logger.info("요청 시작: {}", url);
+
+        webClient.get()
+                .uri(BASE_URL + url)
+                .retrieve()
+                .bodyToMono(String.class)
+                .doOnSubscribe(subscription -> logger.info("요청 등록: {}", url))
+                .doOnNext(response -> logger.info("응답 수신: {} (Counter: {})", url, counter.incrementAndGet()))
+                .doOnSuccess(response -> {
+                    logger.info("요청 완료: {}", url);
+                    latch.countDown();
+                })
+                .subscribe();
+
+        logger.info("메인 스레드 실행 중...");
+        latch.await();
+    }
+
+}
