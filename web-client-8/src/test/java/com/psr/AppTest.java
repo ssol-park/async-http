05d75435a48337b22626aa1afd798eb01301eee5
ssol-park <ksttlov.naver@hanmail.net>
2025-03-17 16:01:40 +0900

    test: WebClient 동작 테스트

--- web-client-8/src/test/java/com/psr/AppTest.java
+++ web-client-8/src/test/java/com/psr/AppTest.java
@@ -23,24 +23,25 @@ import java.util.stream.Stream;
 class AppTest {
     private static final Logger logger = LoggerFactory.getLogger(AppTest.class);
     private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    private static final String DELAY_URL = "https://httpbin.org/delay/2";
     private static final AtomicInteger counter = new AtomicInteger(0);
 
     @Autowired
     private WebClient webClient;
 
-    static Stream<String> provideUrls() {
+    static Stream<String> providePostUrls() {
         return IntStream.rangeClosed(1, 10)
-                .mapToObj(i -> "/posts/" + i);
+                .mapToObj(i -> BASE_URL + "/posts/" + i);
     }
 
     @ParameterizedTest
-    @MethodSource("provideUrls")
+    @MethodSource("providePostUrls")
     void WebClient_테스트(String url) throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
         logger.info("요청 시작: {}", url);
 
         webClient.get()
-                .uri(BASE_URL + url)
+                .uri(url)
                 .retrieve()
                 .bodyToMono(String.class)
                 .doOnSubscribe(subscription -> logger.info("요청 등록: {}", url))
@@ -50,6 +51,48 @@ class AppTest {
                     latch.countDown();
                 })
                 .subscribe();
+
+        logger.info("메인 스레드 실행 중...");
+        latch.await();
+    }
+
+    // TODO ::  Connection Pool, Backpressure, Pending
+    @Test
+    void WebClient_지연_테스트() throws InterruptedException {
+        int reqCnt = 10;
+        CountDownLatch latch = new CountDownLatch(reqCnt);
+        logger.info("#### 요청 시작");
+
+        IntStream.rangeClosed(1, reqCnt).forEach(i -> {
+            String traceId = "REQ-" + i;
+
+            webClient.get()
+                    .uri(DELAY_URL)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .doOnSubscribe(subscription -> logger.info("[{}] 요청 등록 @", traceId))
+                    .doOnNext(response -> logger.info("[{}] 응답 수신 #", traceId))
+                    .doOnSuccess(response -> {
+                        logger.info("[{}] 요청 완료 %", traceId);
+                        latch.countDown();
+                    })
+                    .subscribe();
+        });
+
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            String url = BASE_URL + "/posts/" + i;
+
+            webClient.get()
+                    .uri(url)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .doOnSubscribe(subscription -> logger.info("요청 등록: {}", url))
+                    .doOnNext(response -> logger.info("응답 수신: {} (Counter: {})", url, counter.incrementAndGet()))
+                    .doOnSuccess(response -> {
+                        logger.info("요청 완료: {}", url);
+                    })
+                    .subscribe();
+        });
 
         logger.info("메인 스레드 실행 중...");
         latch.await();
