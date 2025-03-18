8bddd1b6a2794882a268dcd5a875f09dfbfe91e8
ssol-park <ksttlov.naver@hanmail.net>
2025-03-18 16:22:33 +0900

    test: WebClient 지연 요청 테스트

--- web-client-8/src/test/java/com/psr/AppTest.java
+++ web-client-8/src/test/java/com/psr/AppTest.java
@@ -23,7 +23,7 @@ import java.util.stream.Stream;
 class AppTest {
     private static final Logger logger = LoggerFactory.getLogger(AppTest.class);
     private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
-    private static final String DELAY_URL = "https://httpbin.org/delay/2";
+    private static final String DELAY_URL = "https://httpbin.org/delay/%d";
     private static final AtomicInteger counter = new AtomicInteger(0);
 
     @Autowired
@@ -59,15 +59,15 @@ class AppTest {
     // TODO ::  Connection Pool, Backpressure, Pending
     @Test
     void WebClient_지연_테스트() throws InterruptedException {
-        int reqCnt = 10;
-        CountDownLatch latch = new CountDownLatch(reqCnt);
+        int reqSize = 5;
+        CountDownLatch latch = new CountDownLatch(reqSize);
         logger.info("#### 요청 시작");
 
-        IntStream.rangeClosed(1, reqCnt).forEach(i -> {
+        IntStream.rangeClosed(1, reqSize).forEach(i -> {
             String traceId = "REQ-" + i;
 
             webClient.get()
-                    .uri(DELAY_URL)
+                    .uri(String.format(DELAY_URL, i))
                     .retrieve()
                     .bodyToMono(String.class)
                     .doOnSubscribe(subscription -> logger.info("[{}] 요청 등록 @", traceId))
@@ -78,23 +78,8 @@ class AppTest {
                     })
                     .subscribe();
         });
-
-        IntStream.rangeClosed(1, 100).forEach(i -> {
-            String url = BASE_URL + "/posts/" + i;
-
-            webClient.get()
-                    .uri(url)
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .doOnSubscribe(subscription -> logger.info("요청 등록: {}", url))
-                    .doOnNext(response -> logger.info("응답 수신: {} (Counter: {})", url, counter.incrementAndGet()))
-                    .doOnSuccess(response -> {
-                        logger.info("요청 완료: {}", url);
-                    })
-                    .subscribe();
-        });
-
         logger.info("메인 스레드 실행 중...");
+
         latch.await();
     }
 
