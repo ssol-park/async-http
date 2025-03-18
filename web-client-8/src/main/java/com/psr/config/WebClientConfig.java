f2d77774bc28b40499083ec51bc07e1c5a0aaf7a
ssol-park <ksttlov.naver@hanmail.net>
2025-03-18 16:21:16 +0900

    feat: WebClient connection pool 설정 추가

--- web-client-8/src/main/java/com/psr/config/WebClientConfig.java
+++ web-client-8/src/main/java/com/psr/config/WebClientConfig.java
@@ -2,13 +2,28 @@ package com.psr.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        ConnectionProvider connectionProvider = ConnectionProvider.builder("cistom-connection-pool")
+                .maxConnections(100)
+                .pendingAcquireTimeout(Duration.ofSeconds(5))
+                .maxIdleTime(Duration.ofSeconds(30))
+                .build();
+
+        HttpClient httpClient = HttpClient.create(connectionProvider);
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 }
